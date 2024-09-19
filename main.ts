cffa9aa9b5a7ceb9a4a7e10d698ea22c9e40a726
Corbyn-daGama <corbyn.da-gama@mths.ca>
2024-09-19 11:22:41 -0400

    added calculations to find temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Corbyn da Gama
+ * Created on: Sep 2024
+ * This program ... Shows the current temperature
 */
 
-basic.showString('Hello, World!')
+
+
+
+basic.clearScreen()
+basic.pause(1000)
+basic.showIcon(IconNames.Happy)
+
+
+
+// our variable for the temperature
+let currentTemperature = input.temperature()
+
+input.onButtonPressed(Button.A, function () {
+basic.showNumber(currentTemperature)
+})
+basic.showString("The temperature is 25")
+basic.showNumber(25)
